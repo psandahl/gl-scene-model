a5dbc38cab9508defa4a3c3269b1d88e34deb295
Patrik Sandahl <patrik.sandahl@gmail.com>
2017-11-29 15:23:19 +0100

    Skeleton.

---
+++ src/Scene/Model.hs
@@ -0,0 +1,10 @@
+-- |
+-- Module: Scene.Model
+-- Copyright: (c) 2017 Patrik Sandahl
+-- Licence: MIT
+-- Maintainer: Patrik Sandahl <patrik.sandahl@gmail.com>
+-- Stability: experimental
+-- Portability: portable
+module Scene.Model
+    (
+    ) where
